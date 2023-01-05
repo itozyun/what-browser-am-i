1120b2360595b2458c92a8b0178c4de8ec011b25
itozyun <itozyun@gmail.com>
2023-01-05 21:04:38 +0900

    copyright changed.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const gulp            = require('gulp'),
       tempJsName      = 'temp.js',
       tempDir         = require('os').tmpdir() + '/' + moduleName,
       globalVariables = 'document,navigator,screen,parseFloat,Number',
-      copyright       = 'Copyright 2021 itozyun, https://github.com/itozyun/what-browser-am-i, MIT License.';
+      copyright       = '(c) 2021-2023 itozyun, https://github.com/itozyun/what-browser-am-i, MIT License.';
 
       gulp.task('dist', gulp.series(
         function(){
