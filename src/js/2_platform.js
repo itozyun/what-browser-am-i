7fbe89d38d879b4a3897d9a285a7c54a012245ad
itozyun <itozyun@gmail.com>
2024-02-12 04:36:25 +0900

    NDS, NDSi

--- src/js/2_platform.js
+++ src/js/2_platform.js
@@ -3,7 +3,13 @@
  */
 if( strPlatform === 'Nitro' ){
     // https://ja.wikipedia.org/wiki/%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BCDS%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC
+    // https://x.com/itozyun/status/1756757490992328862
     platform         = WHAT_BROWSER_AM_I__PLATFORM_NDS;
+    platformVersion  = 8.5;
+    engine           = WHAT_BROWSER_AM_I__ENGINE_PrestoMobile;
+    engineVersion    = platformVersion;
+    brand            = WHAT_BROWSER_AM_I__BRAND_Opera;
+    brandVersion     = platformVersion;
     deviceTypeIsGame = true;
 } else
 /*----------------------------------------------------------------------------//
@@ -13,7 +19,9 @@ if( strPlatform === 'Nintendo DSi' ){
     // https://ja.wikipedia.org/wiki/%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BCDSi%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC
     // https://developers.whatismybrowser.com/useragents/explore/operating_platform/nintendo-dsi/
     platform         = WHAT_BROWSER_AM_I__PLATFORM_NDSi;
-    platformVersion  = versionOpera; // Opera/9.50 ... Opera/507
+    platformVersion  = versionPresto; // Opera/9.50 ... Opera/507
+    engine           = WHAT_BROWSER_AM_I__ENGINE_PrestoMobile;
+    engineVersion    = platformVersion;
     deviceTypeIsGame = true;
 } else
 /*----------------------------------------------------------------------------//
