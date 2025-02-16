7ff6594c0b82760ce949b27550511d1e978526d7
itozyun <itozyun@gmail.com>
2025-02-16 13:27:33 +0900

    ...

--- packages/i-am/index.d.ts
+++ packages/i-am/index.d.ts
@@ -14,6 +14,7 @@ type HasBased = Common & {
 };
 
 declare namespace iAm {
+    // engine
     const AndroidWebView:Common;
 
     const Chromium:HasBased;
@@ -58,6 +59,76 @@ declare namespace iAm {
     const SafariMobile:Common;
     const iOSWebView:Common;
 
+    // platform
+    const NintendoDS:Common;
+    const NintendoDSi:Common;
+    const Nintendo3DS:Common;
+    const NewNintendo3DS:Common;
+    const Wii:Common;
+    const WiiU:Common;
+    const NintendoSwitch:Common;
+    const PlayStationPortable:Common;
+    const PlayStationVita:Common;
+    const PlayStation3:Common;
+    const PlayStation4:Common;
+    const PlayStation5:Common;
+    const Xbox360:Common;
+    const XboxOne:Common;
+    const XboxSeries:Common;
+    const FeaturePhone:Common;
+    const mylo:Common;
+    const Kobo:Common;
+    const SonyReader:Common;
+    const Kindle:Common;
+    const WindowsCE:Common;
+    const WindowsMobile:Common;
+    const WindowsPhone:Common;
+    const iOS:Common;
+    const iPadOS:Common;
+    const WatchOS:Common;
+    const Android:Common;
+    const AndroidTV:Common;
+    const GoogleTV:Common;
+    const FireOS:Common;
+    const ChromeOS:Common;
+    const FirefoxOS:Common;
+    const KaiOS:Common;
+    const SymbianS60:Common;
+    const MeeGo:Common;
+    const Maemo:Common;
+    const WebOS:Common;
+    const Tizen:Common;
+    const BlackBerry:Common;
+    const Windows16:Common;
+    const Windows:Common;
+    const Mac:Common;
+    const SunOS:Common;
+    const FreeBSD:Common;
+    const OpenBSD:Common;
+    const NetBSD:Common;
+    const Ubuntu:Common;
+    const Mint:Common;
+    const Fedora:Common;
+    const Gentoo:Common;
+    const Linux:Common;
 
-
+    // device
+    const PlayStation:Common;
+    const iPhone:Common;
+    const iPod:Common;
+    const iPad:Common;
+    const BOOX:Common;
+    const Cybook:Common;
+    const eClicto:Common;
+    const Fidibook:Common;
+    const FireTV:Common;
+    const Hisense:Common;
+    const icarus:Common;
+    const KindleFire:Common;
+    const Likebook:Common;
+    const Manta:Common;
+    const Movistar:Common;
+    const Nook:Common;
+    const PocketBook:Common;
+    const Tolino:Common;
 };
